fe51cfb29cb8f56595dfc5511c626f02948eed0f
AunJai Official <aunjai.dev@gmail.com>
2022-04-02 17:44:06 +0700

    no message

---
+++ lib/utils/widgets/dialog_custom.dart
@@ -0,0 +1,130 @@
+import 'package:aunjai/constant/style_constant.dart';
+import 'package:aunjai/utils/helper.dart';
+import 'package:aunjai/utils/widgets/card_custom/card.dart';
+import 'package:aunjai/utils/widgets/horizontal.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Widget _weatherTemplate(context) {
+  return SizedBox(
+    width: Helper.getScreenWidth(context),
+    height: 500,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          children: [
+            Icon(Icons.location_pin),
+            Horizontal(10.0),
+            normalText("Roi-et, Thailand")
+          ],
+        ),
+        Helper.readPngPicture("assets/images/cloudy-night.png", width: 200.0),
+        normalText("25°"),
+        normalText("Thunderstorm"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(
+              children: [
+                Icon(FontAwesomeIcons.wind),
+                normalText("10 km/h"),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(FontAwesomeIcons.cloudMoonRain),
+                normalText("60%"),
+              ],
+            )
+          ],
+        ),
+        SizedBox(
+          width: Helper.getScreenWidth(context),
+          height: 200,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              CardCustom(),
+              CardCustom(),
+              CardCustom(),
+              CardCustom(),
+              CardCustom(),
+              CardCustom(),
+              CardCustom(),
+              CardCustom(),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget _openTimeTemplate(context){
+  return Container(
+    width: Helper.getScreenWidth(context),
+    height: 500,
+    child: Column(
+      children: [
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+        Row(
+          children: [
+            normalText("Monday"),
+            normalText("08.00PM -08.00AM"),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Future<void> openDialog(BuildContext context, String type) async {
+  Widget temp = Container();
+  if (type == "weatherTemplate") {
+    temp = _weatherTemplate(context);
+  }else if(type == "openTimeTemplate"){
+    temp =_openTimeTemplate(context);
+  }
+
+  return showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        content: SingleChildScrollView(child: temp),
+      ));
+}
--- lib/views/app_main/attraction/attraction_place/widgets/about_section.dart
+++ lib/views/app_main/attraction/attraction_place/widgets/about_section.dart
@@ -4,6 +4,7 @@ import 'package:aunjai/routes.dart';
 import 'package:aunjai/utils/helper.dart';
 import 'package:aunjai/utils/widgets/card_custom/card.dart';
 import 'package:aunjai/utils/widgets/decoration.dart';
+import 'package:aunjai/utils/widgets/dialog_custom.dart';
 import 'package:aunjai/utils/widgets/get_rating_star.dart';
 import 'package:aunjai/utils/widgets/horizontal.dart';
 import 'package:flutter/material.dart';
@@ -12,74 +13,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class AboutSection extends StatelessWidget {
   const AboutSection({Key? key}) : super(key: key);
 
-  Widget weatherTemplate(context) {
-    return Container(
-      width: Helper.getScreenWidth(context),
-      height: 500,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              Icon(Icons.location_pin),
-              Horizontal(10.0),
-              normalText("Roi-et, Thailand")
-            ],
-          ),
-          Helper.readPngPicture("assets/images/cloudy-night.png", width: 200.0),
-          normalText("25°"),
-          normalText("Thunderstorm"),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                children: [
-                  Icon(FontAwesomeIcons.wind),
-                  normalText("10 km/h"),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(FontAwesomeIcons.cloudMoonRain),
-                  normalText("60%"),
-                ],
-              )
-            ],
-          ),
-          SizedBox(
-            width: Helper.getScreenWidth(context),
-            height: 200,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                CardCustom(),
-                CardCustom(),
-                CardCustom(),
-                CardCustom(),
-                CardCustom(),
-                CardCustom(),
-                CardCustom(),
-                CardCustom(),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
 
-  Future<void> openDialog(BuildContext context, String type) async {
-    Widget temp = Container();
-    if (type == "weatherTemplate") {
-      temp = weatherTemplate(context);
-    }
-
-    return showDialog(
-        context: context,
-        builder: (ctx) => AlertDialog(
-              content: SingleChildScrollView(child: temp),
-            ));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,7 +49,9 @@ class AboutSection extends StatelessWidget {
               prefixIcon: FontAwesomeIcons.calendar,
               label: "Open",
               subContent: "8:30 PM - 10:00 PM",
-              onTap: () {}),
+              onTap: () {
+                openDialog(context, "openTimeTemplate");
+              }),
           getButtonDetail(
               subContent: "14 Phan Huy Ich, Ba Dinh, Hanoi",
               prefixIcon: Icons.location_pin,
